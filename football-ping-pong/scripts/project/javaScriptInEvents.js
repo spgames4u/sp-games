ff35b976692a25bcaf018507f547b2a584d1e324
العاب سكور بوينت <spoints.games@gmail.com>
2026-02-20 09:43:20 +0300

    بونغ

---
+++ football-ping-pong/scripts/project/javaScriptInEvents.js
@@ -0,0 +1,16 @@
+
+
+const scriptsInEvents = {
+
+	async GameEvents_Event69_Act2(runtime, localVars)
+	{
+		ctlArcadeSaveScore(1000);
+	},
+
+	async GameEvents_Event69_Act5(runtime, localVars)
+	{
+		ctlArcadeSaveScore(1000);
+	}
+};
+
+globalThis.C3.JavaScriptInEvents = scriptsInEvents;
